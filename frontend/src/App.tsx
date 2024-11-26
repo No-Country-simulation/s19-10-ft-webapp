b78d148d72fa441bfe0867a1a62fe064ab7d2dc5
Heba-Webdev23 <jtvonline@yahoo.com>
2024-11-26 23:04:44 +0300

    chore: maintenance tasks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,11 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-'>Mini Clon Notebook</h1>
+    <div className=''>
 
-    </>
+      <h1 className=''>Mini Clon Notebook</h1>
+
+    </div>
   )
 }
 
