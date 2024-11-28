b700c525e74cdfdeabeaaa1cc5c69b45f8772ef3
Heba-Webdev23 <jtvonline@yahoo.com>
2024-11-28 13:25:58 +0300

    feat: react toastify container and react query provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import Router from './routes/index.tsx'
 import './index.css'
+import 'react-toastify/dist/ReactToastify.css';
+import ReactQueryProvider from './util/react-query.provider.tsx';
+import { ToastContainer } from 'react-toastify';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <>
-    <Router />
-  </>,
-)
+    <ReactQueryProvider>
+      <Router />
+      <ToastContainer />
+    </ReactQueryProvider>
+  </>
+);
