cae673b515ee0d8e0528013107da34c8312a20b5
Cristian Dizeo <dizeoc@gmail.com>
2024-12-06 09:41:12 -0300

    fix: landingpage imgs

--- frontend/src/modules/home/Landingpage.tsx
+++ frontend/src/modules/home/Landingpage.tsx
@@ -1,21 +1,23 @@
 import { Link } from 'react-router-dom'
-import logo from '../../assets/common/logo-title.svg'
+import logo from '@/assets/common/logo-title.svg'
 
 export default function Landingpage() {
 
   return (
     <div className=''>
-      <div className="h-screen bg-[url('@/assets/landing-bg.jpg')] flex flex-col justify-between">
+      <div className="h-screen bg-[url('@/assets/dashboard/landing-bg.jpg')] flex flex-col justify-between">
         {/* Header */}
-        <header className="w-full p-2 flex justify-between items-center bg-slate-50">
+        <header className="w-full p-2 flex justify-between items-center">
+          <div className='bg-white py-1 rounded-full text-lg font-medium shadow-md transition duration-300'>
         <Link to="/">
           <img src={logo} alt="notepad ai" className="p-4  w-44" />
           </Link>
+          </div>
           <nav className="space-x-4 text-black">
-            <Link to="/login" className="bg-blue-600 text-white hover:bg-blue-500 p-3 rounded">
+            <Link to="/login" className="bg-blue-600 hover:bg-blue-500 text-white px-6 py-3 rounded-full text-lg font-medium shadow-md transition duration-300">
               Sign In
             </Link>
-            <Link to="/signup" className="bg-blue-600 text-white hover:bg-blue-500 p-3 rounded">
+            <Link to="/signup" className="bg-blue-600 hover:bg-blue-500 text-white px-6 py-3 rounded-full text-lg font-medium shadow-md transition duration-300">
               Sign up
             </Link>
           </nav>
@@ -29,7 +31,7 @@ export default function Landingpage() {
           </p>
           <Link
             to="/dashboard"
-            className="bg-blue-700 hover:bg-blue-500 text-white px-6 py-3 rounded-full text-lg font-medium shadow-md transition duration-300"
+            className="bg-blue-600 hover:bg-blue-500 text-white px-6 py-3 rounded-full text-lg font-medium shadow-md transition duration-300"
           >
             Get Started
           </Link>
